6a7977dcfe137a0930e6feccacdcd111f0acda9a
Arnold <antaloczi.arnold@gmail.com>
2020-12-11 21:22:17 +0100

    Updated logic and some trifle

--- js/app.js
+++ js/app.js
@@ -1,30 +1,92 @@
 "use strict";
 
+const db = {
+    createPendingItemsContainer() { if (!localStorage.getItem("Pending-items")) { localStorage.setItem("Pending-items", "{}") } },
+    createCompletedTasksContainer() { if (!localStorage.getItem("Completed-tasks")) { localStorage.setItem("Completed-tasks", "{}") } },
+    getPendingItems() { return JSON.parse(localStorage.getItem("Pending-items")) },
+    setPendingItems(pendingItemsObjectFormat) { localStorage.setItem("Pending-items", JSON.stringify(pendingItemsObjectFormat)) },
+    addPendingItem(key, value) {
+        const pendingItems = this.getPendingItems();
+        pendingItems[key] = value;
+        this.setPendingItems(pendingItems);
+    },
+    countPendingItems() { return todoObject.padNumber(Object.keys(this.getPendingItems()).length + 1) },
+    countPendingItemsWithoutPad() { return Object.keys(this.getPendingItems()).length },
+
+    getCompletedTasks() { return JSON.parse(localStorage.getItem("Completed-tasks")) },
+    setCompletedTasks(completedTasksObjectFormat) { localStorage.setItem("Completed-tasks", JSON.stringify(completedTasksObjectFormat)) },
+    addCompletedTask(key, value) {
+        const completedTasks = this.getCompletedTasks();
+        completedTasks[key] = value;
+        this.setCompletedTasks(completedTasks);
+    },
+    countCompletedTasks() { return todoObject.padNumber(Object.keys(this.getCompletedTasks()).length + 1) },
+};
+
 const todoObject = {
-    pendingItemsCounter: Object.values(localStorage).length,
-    numberPad(number) { return number < 10 ? `0${number}` : number },
+    pendingItemsCounter: 0,
+    padNumber(number) { return number < 10 ? `0${number}` : number },
     nowDate() { return new Date() },
     getInputField() { return document.querySelector(".input-field") },
     getAddButton() { return document.querySelector(".add-btn") },
     getPendingItemsContainer() { return document.querySelector(".pending-items-container") },
+    getCompletedTasksContainer() { return document.querySelector(".completed-tasks-container") },
     getBottomButtons() { return document.querySelector(".bottom-buttons-container") },
-    addItemToLocalStorage(item_text) { localStorage.setItem(this.pendingItemsCounter, item_text) },
-    getPendingItems() { return Object.keys(localStorage).sort().map(k => localStorage.getItem(k)) },
+    getPendingItems() { return document.querySelectorAll(".pending-item") },
+    getPendingItemIdKey(pendingItem) { return pendingItem.querySelector("input").getAttribute("id").split("-")[1] },
+    getPendingItemText(pendingItem) { return pendingItem.querySelector("label").textContent },
+    addAndRemoveDeleteButton() {
+        Array.from(this.getPendingItems()).map(item => {
+            item.addEventListener("mouseover", () => item.classList.add("show-delete-btn"));
+            item.addEventListener("mouseleave", () => item.classList.remove("show-delete-btn"));
+        });
+    },
+    getItemFromId(id) { return this.getPendingItemsContainer().querySelector(`#${id}`) },
+    movePendingItemToCompletedTasksContainer(pendingItem) {
+        const currentItemKey = todoObject.getPendingItemIdKey(pendingItem);
+
+        const pendingItems = db.getPendingItems();
+        delete pendingItems[currentItemKey];
+        db.setPendingItems(pendingItems);
+
+        const completedTasks = db.getCompletedTasks();
+        completedTasks[currentItemKey] = todoObject.getPendingItemText(pendingItem);
+        db.setCompletedTasks(completedTasks);
+
+        Array.from(this.getPendingItems()).map(item => {
+            if (item === pendingItem) {
+                item.style.display = "none";
+            }
+        });
+
+        pendingItem.style.display = "block";
+        this.getCompletedTasksContainer().appendChild(pendingItem);
+
+    },
+    checkboxOnChange() {
+        const currentCheckbox = this;
+        const pendingItem = this.parentElement;
+        todoObject.movePendingItemToCompletedTasksContainer(pendingItem);
+        todoObject.setPendingItemsLabel();
+    },
     insertItemToContainer(text, index) {
         const pendingItem = `<div class="pending-item">
                                 <input type="checkbox" id="item-${index}">
                                 <label for="item-${index}">${text}</label>
                                 <button class="delete-btn">X</button>
-                            </div>`
+                            </div>`;
         this.getPendingItemsContainer().insertAdjacentHTML("afterbegin", pendingItem);
+        const currentCheckbox = this.getItemFromId(`item-${index}`);
+        currentCheckbox.addEventListener("change", this.checkboxOnChange);
+        this.addAndRemoveDeleteButton();
     },
     hideBottomButtons() { this.getBottomButtons().style.display = "none" },
     showBottomButtons() { this.getBottomButtons().style.display = "block" },
     setPendingItemsLabel() {
         const label = document.querySelector(".pending-items-label");
-        label.textContent = `You have ${todoObject.pendingItemsCounter} pending items`;
+        db.countPendingItemsWithoutPad() === 0 ? label.textContent = "" :
+            label.textContent = `You have ${db.countPendingItemsWithoutPad()} pending items`;
     },
-
 };
 
 // Set weekday and date
@@ -35,8 +97,8 @@ const getWeekDay = (() => {
     weekDayDiv.textContent = weekDay;
 })();
 const getDate = (() => {
-    const month = todoObject.numberPad(todoObject.nowDate().getMonth() + 1);
-    const day = todoObject.numberPad(todoObject.nowDate().getDate());
+    const month = todoObject.padNumber(todoObject.nowDate().getMonth() + 1);
+    const day = todoObject.padNumber(todoObject.nowDate().getDate());
     const year = todoObject.nowDate().getFullYear();
     const date = `${month}-${day}-${year}`;
     const dateDiv = document.querySelector(".date");
@@ -45,20 +107,27 @@ const getDate = (() => {
 
 // Check local storage
 const checkLocalStorage = (() => {
-    if (Object.keys(localStorage).length) {
-        Array.from(todoObject.getPendingItems()).map((item, index) => todoObject.insertItemToContainer(item, index));
+    db.createPendingItemsContainer();
+    db.createCompletedTasksContainer();
+
+    if (Object.keys(db.getPendingItems()).length) {
+        for (let pendingItem in db.getPendingItems()) {
+            const pendingItemText = db.getPendingItems()[pendingItem];
+            todoObject.insertItemToContainer(pendingItemText, pendingItem)
+        }
         todoObject.showBottomButtons();
         todoObject.setPendingItemsLabel();
     } else {
         todoObject.hideBottomButtons();
     }
+
 })();
 
 const addButtonHandler = () => {
     const pendingItem = todoObject.getInputField();
     todoObject.pendingItemsCounter++;
-    todoObject.addItemToLocalStorage(pendingItem.value);
-    todoObject.insertItemToContainer(pendingItem.value);
+    db.addPendingItem(db.countPendingItems(), pendingItem.value);
+    todoObject.insertItemToContainer(pendingItem.value, todoObject.padNumber(todoObject.pendingItemsCounter));
     todoObject.setPendingItemsLabel();
     todoObject.showBottomButtons();
     pendingItem.value = "";
